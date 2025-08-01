981085f65dd8e95c3d51f5546d4cec69d72d3148
Andersson Camilo Cardenas Guarin <anderssonccg@gmail.com>
2025-08-01 15:57:40 -0500

    feat: add status to profile card

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,19 @@ function App() {
       name: "Andres",
       role: "Estudiante",
       img: "https://i.pravatar.cc/100",
+      isActive: true,
     },
     {
       name: "Alessandro",
       role: "Estudiante",
       img: "https://i.pravatar.cc/100",
+      isActive: false,
     },
     {
       name: "Mauricio",
       role: "Estudiante",
       img: "https://i.pravatar.cc/100",
+      isActive: true,
     },
   ];
 
@@ -24,8 +27,16 @@ function App() {
     <>
       <h1 className={styles.title}>Listado de estudiantes</h1>
       <section className={styles.container}>
-        {students.map(({ name, role, img }, index) => {
-          return <Card key={index} name={name} role={role} image={img} />;
+        {students.map(({ name, role, img, isActive }, index) => {
+          return (
+            <Card
+              key={index}
+              name={name}
+              role={role}
+              image={img}
+              isActive={isActive}
+            />
+          );
         })}
       </section>
     </>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
 import styles from "./Card.module.css";
 
-export const Card = ({ name, role, image }) => {
+export const Card = ({ name, role, image, isActive }) => {
   return (
     <>
       <div className={styles.container}>
         <img className={styles.img} src={image} alt="" />
         <h2 className={styles.name}>{name}</h2>
         <strong className={styles.role}> {role}</strong>
+        <div className={styles.statusContainer}>
+          <span>Estado: </span>
+          <strong className={isActive ? styles.active : styles.inactive}>
+            {isActive ? "ACTIVO" : "INACTIVO"}
+          </strong>
+        </div>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
           dolore minima qui sapiente doloremque similique! Praesentium, ducimus
